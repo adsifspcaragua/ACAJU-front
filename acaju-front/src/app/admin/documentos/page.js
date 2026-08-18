be9e5d0478f6196d290bba7644ecb00f74da970b
kevin4bmael <kevin4bmael@gmail.com>
2026-08-18 01:23:20 -0300

    correção de espaçamento

--- acaju-front/src/app/admin/documentos/page.js
+++ acaju-front/src/app/admin/documentos/page.js
@@ -59,16 +59,21 @@ export default function RepositorioDocumentos() {
 const styles = {
   container: {
     display: "flex",
-    minHeight: "100vh",
+    height: "100vh",
+    width: "100vw",
+    overflow: "hidden", 
     margin: 0,
+    backgroundColor: "transparent", 
   },
   mainContent: {
     flexGrow: 1,
+    overflowY: "auto", 
     padding: "50px",
     boxSizing: "border-box",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    marginLeft: "300px",
   },
   headerContainer: {
     width: "100%",
